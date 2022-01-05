b7124bab325ea9e28c98f45d557f8b3fa93f0f79
Enno Liermann <Ennoliermann@web.de>
2022-01-05 19:27:44 +0100

    recreation of BNO object on power loss added

--- Adafruit Feather/deploy/BLE_IMU/src/BLE_IMU.ino
+++ Adafruit Feather/deploy/BLE_IMU/src/BLE_IMU.ino
@@ -18,24 +18,51 @@ uint8_t connection_interval_max = 20; //20 * 1.25ms = 25ms
 uint8_t connection_handle;
 
 SoftwareTimer dataTimer;
-// SoftwareTimer batteryTimer;
+SoftwareTimer BNOWatchdog;
 
-Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);//low //Adafruit_BNO055(56, 0x29)//high//Adafruit_BNO055(55);
+Adafruit_BNO055 *bno;
 
 Adafruit_NeoPixel neopixel = Adafruit_NeoPixel(NEOPIXEL_NUM, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
 
+const String BNO_System_Status[7] = {
+     "Idle",
+     "System Error",
+     "Initializing Peripherals",
+     "System Iniitalization",
+     "Executing Self-Test",
+     "Sensor fusio algorithm running",
+     "System running without fusion algorithms"};
 
+const String BNO_Self_test[8] = {
+     "Accelerometer self test",
+     "Magnetometer self test",
+     "Gyroscope self test",
+     "MCU self test", "", "", "", ""};
+
+const String BNO_Error[11] = {
+     "No error",
+     "Peripheral initialization error",
+     "System initialization error",
+     "Self test result failed",
+     "Register map value out of range",
+     "Register map write error",
+     "BNO low power mode not available for selected operat ion mode",
+     "Accelerometer power mode not available",
+     "Fusion algorithm configuration error",
+     "Sensor configuration error"};
 
 void setup() 
 {
   Serial.begin(115200);
 
-  if(!bno.begin())
+  bno = new Adafruit_BNO055(55, 0x28);//low //Adafruit_BNO055(56, 0x29)//high//Adafruit_BNO055(55);
+
+  if(!bno->begin())
   {
     Serial.println("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
     while(1);
   }
-  bno.setExtCrystalUse(true);
+  bno->setExtCrystalUse(true);
 
   Bluefruit.autoConnLed(true);
   Bluefruit.configPrphBandwidth(BANDWIDTH_HIGH);
@@ -59,12 +86,13 @@ void setup()
   blebas.begin();
   blebas.notify(100);
 
-  int* id = new int(2); 
+  // int* id = new int(2); 
 
-  // batteryTimer.begin(27,SendBattery,0,true);
+  BNOWatchdog.begin(3000,getStatus,0,true);
   // delay(1);
   dataTimer.begin(10,SendData,0,true);
 
+  // BNOWatchdog.start();
   
 
   neopixel.begin();
@@ -122,11 +150,12 @@ void onDisconnect(uint16_t conn_handle, uint8_t reason)
 char sendBuffer[16] = {'A','0','0','0','1','0','1','2','0','0','1','0','1','2'};
 char tmp[16];
 uint8_t sys, gyro, accel, mag = 0;
+uint8_t system_status, self_test_result, system_error = 0;
 char calib = 0x00;
 uint8_t bufferQuat[8];
 uint8_t bufferVect[6];
 
-
+bool read = true;
 
 void loop() 
 {
@@ -135,26 +164,72 @@ void loop()
     memset(bufferQuat, 0, 8);
     memset(bufferVect, 0, 6);
 
-    bno.getCalibration(&sys, &gyro, &accel, &mag);
+
+
+    bno->getCalibration(&sys, &gyro, &accel, &mag);
     calib = 0x00;
     calib = ( mag & 0x03)| ( (accel <<2)& 0x0C) | ( (gyro <<4)& 0x30)| ( (sys <<6)& 0xC0);
-    bno.readLen(Adafruit_BNO055::BNO055_QUATERNION_DATA_W_LSB_ADDR, bufferQuat, 8);
-    
+    if(!bno->readLen(Adafruit_BNO055::BNO055_QUATERNION_DATA_W_LSB_ADDR, bufferQuat, 8))   // Power cut off?
+    {
+      Serial.println("1");
+      int errCount = 0;
+      while(true)
+      {
+        errCount++;
+        if(bno->readLen(Adafruit_BNO055::BNO055_QUATERNION_DATA_W_LSB_ADDR, bufferQuat, 8))
+          if(elementsEqual(bufferQuat,8) || errCount >= 50)
+          {
+            Serial.println("creating new BNO objekt");
+            delete bno;
+            bno = new Adafruit_BNO055(55, 0x28);//low //Adafruit_BNO055(56, 0x29)//high//Adafruit_BNO055(55);
+
+            if(!bno->begin())
+            {
+              Serial.println("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+              while(1);
+            }
+            bno->setExtCrystalUse(true);
+            break;
+          }
+          else
+          {
+            break;
+          }
+        delay(10);
+      }
+        Serial.print("Error Count: ");
+        Serial.println(errCount);
+      }
+      // getStatus(0);
+      
+      // BNOWatchdog.start();
+      // bno.getSystemStatus(&system_status,0,0);
+      // if(system_status == 0)                          // Reconnect 
+      // {
+      //   Serial.println("3");
+      //   if(!bno.begin())
+      //   {
+      //     Serial.println("Restart not possible");
+      //   }
+          
+      // }
+          
     tmp[0] = '0';
-    tmp[1] = calib;
+    tmp[1] = sys;
     for(int i =0;i<8;i++)
       tmp[i+2] = bufferQuat[i];
 
-    bno.readLen(Adafruit_BNO055::BNO055_GYRO_DATA_X_LSB_ADDR, bufferVect, 6);
+    bno->readLen(Adafruit_BNO055::BNO055_GYRO_DATA_X_LSB_ADDR, bufferVect, 6);
     for(int i =0;i<6;i++)
       tmp[i+10] = bufferVect[i]; //11-16
-    
 
     memcpy(&sendBuffer[0],&tmp[0],sizeof(sendBuffer));
     // Serial.print("Data: ");
     // Serial.println(tmp);
 
     delay(5);
+
+    
 }
 
 void bleuart_rx_callback(uint16_t conn_hdl)
@@ -186,34 +261,84 @@ void SendData(TimerHandle_t handle)
   digitalToggle(LED_RED);
 }
 
-// void SendBattery(TimerHandle_t handle)
-// { 
-//   Serial.println("1");
-//   Serial.println("2");
-//   // int r = rand() % 100;
-//   // blebas.write(r);
-//   // Serial.println("2");
-//   // uint32_t c = neopixel.Color(0x00, 0x00, 254);
-//   // neopixel.fill(c, 0, NEOPIXEL_NUM);
-//   // neopixel.show();
-//   // Serial.println("3");
-//   // delay(1);
-//   // Serial.println("4");
-//   // c = neopixel.Color(0x00, 0x00, 0x00);
-//   // Serial.println("5");
-//   // neopixel.fill(c, 0, NEOPIXEL_NUM);
-//   // Serial.println("6");
-//   // neopixel.show();
-//   // Serial.println("7");
 
-//   // digitalToggle(LED_RED);
-// }
+  // void getSystemStatus(uint8_t *system_status, uint8_t *self_test_result,
+  //                      uint8_t *system_error);
+void getStatus(TimerHandle_t handle)
+{
+  bool bin[8];
+  
+  bno->getSystemStatus(&system_status, &self_test_result, &system_error);
+
+  toBin(bin, self_test_result);
+
+  Serial.print("System status: ");
+  Serial.println(BNO_System_Status[system_status]);
+
+  Serial.print("Self test result: ");
+  if (self_test_result == 15)
+    Serial.println("All Good!");
+  else
+  {
+    for(int i = 0; i < 4; i++)
+    {
+      if(bin[i])
+        Serial.print(BNO_Self_test[i]);
+    }
+    Serial.println();
+  }
+
+  Serial.print("System error: ");
+  Serial.println(BNO_Error[system_error]);
+}
+
+void toBin(bool arr[],uint8_t n)
+{
+  for (int i = 7; i >= 0; --i) 
+  {
+      arr[i] = (n >> i) & 1;
+  }
+}
+
+bool elementsEqual(uint8_t arr[],uint8_t len)
+{
+  for (int i = 0; i < len-1; ++i) 
+    if(arr[i] != arr[i++])
+      return false;
+  return true;
+}
+// const String BNO_System_Status[7] = {
+//      "Idle",
+//      "System Error",
+//      "Initializing Peripherals",
+//      "System Iniitalization",
+//      "Executing Self-Test",
+//      "Sensor fusio algorithm running",
+//      "System running without fusion algorithms"};
+
+// const String BNO_System_Status[8] = {
+//      "Accelerometer self test",
+//      "Magnetometer self test",
+//      "Gyroscope self test",
+//      "MCU self test", "", "", "", ""};
+
+// const String BNO_System_Status[11] = {
+//      "No error",
+//      "Peripheral initialization error",
+//      "System initialization error",
+//      "Self test result failed",
+//      "Register map value out of range",
+//      "Register map write error",
+//      "BNO low power mode not available for selected operat ion mode",
+//      "Accelerometer power mode not available",
+//      "Fusion algorithm configuration error",
+//      "Sensor configuration error"};
+
 
 void bleuart_notify_callback(uint16_t conn_hdl, bool enabled)
 {
   Serial.println("Notify ON!");
   
   // delay(1);
-  // batteryTimer.start();
   dataTimer.start();
 }
