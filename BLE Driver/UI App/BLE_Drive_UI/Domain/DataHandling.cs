802df566f21b9944c1b0c657266067b97e19f079
Enno Liermann <ennoliermann@web.de>
2022-01-30 14:52:30 +0100

    Fixed bug: Savingthread was sometimes still running on recreation of pollingthread

--- BLE Driver/UI App/BLE_Drive_UI/Domain/DataHandling.cs
+++ BLE Driver/UI App/BLE_Drive_UI/Domain/DataHandling.cs
@@ -58,231 +58,7 @@ namespace BLE_Drive_UI.Domain
         }
     }
 
-    //public class SyncDataSaver
-    //{
-    //    private static String _path;
-    //    private static UInt16 _bufferLength;
-
-    //    private StringBuffer buffer1;
-    //    private StringBuffer buffer2;
-
-    //    private static object mThreadLock = new object();
-
-    //    private Thread Buffer1PollingThread;
-    //    private Thread Buffer2PollingThread;
-    //    private Stopwatch Watch;
-
-    //    private static double _rate;
-    //    private double cummulativeRate;
-
-    //    private String dataToSave = String.Empty;
-
-    //    public bool isSaving = false;
-
-    //    public bool Active = false;
-
-    //    public SyncDataSaver(UInt16 bufferLength, double rate)
-    //    {
-
-    //        var dir = Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).FullName).FullName;
-    //        Console.WriteLine(dir);
-
-    //        _path = dir + @"\Data\";
-
-    //        if (!Directory.Exists(_path))
-    //            Directory.CreateDirectory(_path);
-
-    //        _bufferLength = bufferLength;
-    //        _rate = rate;
-
-    //        buffer1 = new StringBuffer(_bufferLength);
-    //        buffer2 = new StringBuffer(_bufferLength);
-
-    //        buffer1.Active = true;
-    //        buffer2.Active = false;
-
-    //        Watch = new Stopwatch();
-
-    //    }
-
-    //    public void start()
-    //    {
-    //        Console.WriteLine("Start Saving....");
-    //        if (Buffer1PollingThread != null || Buffer2PollingThread != null)
-    //        {
-    //            while (Buffer1PollingThread.IsAlive || Buffer2PollingThread.IsAlive)
-    //            {
-    //                Console.WriteLine(" Threads still alive....");
-    //                Thread.Sleep(200);
-    //            }
-    //        }
-    //        dataToSave = String.Empty;
-    //        Active = true;
-    //        isSaving = true;
-    //        buffer1.Active = true;
-    //        buffer2.Active = false;
-
-    //        cummulativeRate = 0;
-
-    //        Buffer1PollingThread = new Thread(Buffer1Polling);
-    //        Buffer2PollingThread = new Thread(Buffer2Polling);
-
-    //        Buffer1PollingThread.Start();
-    //        Buffer2PollingThread.Start();
-
-
-
-    //        Console.WriteLine("PollingThread started....");
-    //    }
-
-    //    public void stop()
-    //    {
-    //        isSaving = false;
-    //        //dataToSave = String.Empty;
-    //        flush();
-    //        Watch.Reset();
-    //        while (Buffer1PollingThread.IsAlive || Buffer2PollingThread.IsAlive) {; }     //Wait for Threads to finish
-    //        Active = false;
-    //    }
-
-
-    //    public void addData(String data)
-    //    {
-    //        lock (mThreadLock)
-    //        {
-    //            dataToSave = data;
-    //        }
-    //    }
-
-    //    private void Buffer1Polling()
-    //    {
-    //        while (dataToSave == String.Empty) { Thread.Sleep(5); }
-    //        Watch.Start();
-    //        while (isSaving)
-    //        {
-    //            if (!buffer1.Active) { var n = _rate / 5; Thread.Sleep((int)n); continue; }
-    //            double t = Watch.Elapsed.TotalMilliseconds;
-    //            if (t >= cummulativeRate)
-    //            {
-    //                cummulativeRate += _rate;
-    //                String timestamp = (t / 1000).ToString("0.0000");
-    //                lock (mThreadLock)
-    //                {
-    //                    buffer1.Add(timestamp + dataToSave);
-    //                }
-    //                if (buffer1.Count >= _bufferLength)
-    //                {
-    //                    buffer1.Active = false;
-    //                    buffer2.Active = true;
-    //                    save(buffer1);
-    //                }
-    //            }
-    //        }
-    //    }
-
-    //    private void Buffer2Polling()
-    //    {
-    //        while (isSaving)
-    //        {
-    //            if (!buffer2.Active) { var n = _rate / 5; Thread.Sleep((int)n); continue; }
-    //            double t = Watch.Elapsed.TotalMilliseconds;
-    //            if (t >= cummulativeRate)
-    //            {
-    //                cummulativeRate += _rate;
-    //                String timestamp = (t / 1000).ToString("0.0000");
-    //                lock (mThreadLock)
-    //                {
-    //                    buffer2.Add(timestamp + dataToSave);
-    //                }
-    //                if (buffer2.Count >= _bufferLength)
-    //                {
-    //                    buffer1.Active = true;
-    //                    buffer2.Active = false;
-    //                    save(buffer2);
-    //                }
-    //            }
-    //        }
-    //    }
-
-    //    //private void dataPolling()
-    //    //{
-    //    //    Console.WriteLine("Start Polling");
-    //    //    double rate = _rate;
-    //    //    while (isSaving)
-    //    //    {
-    //    //        double t = Watch.Elapsed.TotalMilliseconds;
-    //    //        if(t >= rate)
-    //    //        {
-    //    //            //Console.WriteLine("5 ms");
-    //    //            String timestamp = t.ToString("0.0000");
-    //    //            if(buffer1.Active)
-    //    //            {
-    //    //                lock (mThreadLock)
-    //    //                {
-    //    //                    buffer1.Add(timestamp + dataToSave);
-    //    //                }
-    //    //                if (buffer1.Count >= _bufferLength && !buffer1.Busy)
-    //    //                {
-    //    //                    SavingThread.Start();
-    //    //                }
-    //    //            }
-    //    //            else if (buffer2.Active)
-    //    //            {
-    //    //                lock (mThreadLock)
-    //    //                {
-    //    //                    buffer2.Add(timestamp + dataToSave);
-    //    //                }
-    //    //                if (buffer2.Count >= _bufferLength && !buffer2.Busy)
-    //    //                {
-    //    //                    SavingThread.Start();
-    //    //                }
-    //    //            }
-    //    //            else
-    //    //            {
-    //    //                throw new Exception("No Buffer active in data saving class polling function");
-    //    //            }
-    //    //            rate += _rate;
-    //    //        }
-
-    //    //    }
-    //    //}
-
-    //    private bool busy = false;
-    //    private void save(StringBuffer buffer)
-    //    {
-    //        busy = true;
-    //        String filename = DateTime.Now.ToString("yyyy_MM_dd_HH_mm_ss");
-    //        File.WriteAllLines(_path + filename + ".txt", buffer.Flush());
-    //        busy = false;
-    //        //if (i == 1)
-    //        //{
-    //        //    File.WriteAllLines(_path + filename + ".txt", buffer1.Flush());
-    //        //}
-    //        //else if (i == 2)
-    //        //{
-    //        //    File.WriteAllLines(_path + filename + ".txt", buffer2.Flush());
-    //        //}
-    //        //else
-    //        //{
-    //        //    throw new Exception("No Buffer active in data saving Class Save function");
-    //        //}
-    //    }
-
-    //    private void flush()
-    //    {
-    //        while (busy) { Thread.Sleep(20); }
-    //        if (buffer1.Active)
-    //        {
-    //            save(buffer1);
-    //        }
-    //        else if (buffer2.Active)
-    //        {
-    //            save(buffer2);
-    //        }
-    //        //while (SavingThread.IsAlive) { }
-    //        //SavingThread.Start();
-    //    }
-    //}
+ 
     public class SyncDataSaver
     {
         private static String _path;
@@ -333,14 +109,7 @@ namespace BLE_Drive_UI.Domain
         public void start()
         {
             Console.WriteLine("Start Saving....");
-            if (Buffer1PollingThread != null || Buffer2PollingThread != null)
-            {
-                while (Buffer1PollingThread.IsAlive || Buffer2PollingThread.IsAlive)
-                {
-                    Console.WriteLine(" Threads still alive....");
-                    Thread.Sleep(200);
-                }
-            }
+            waitForThreadsToFinish();
             dataToSave = String.Empty;
             Active = true;
             isSaving = true;
@@ -366,7 +135,7 @@ namespace BLE_Drive_UI.Domain
             //dataToSave = String.Empty;
             flush();
             Watch.Reset();
-            while (Buffer1PollingThread.IsAlive || Buffer2PollingThread.IsAlive) {; }     //Wait for Threads to finish
+            waitForThreadsToFinish();
             Active = false;
         }
 
@@ -433,6 +202,35 @@ namespace BLE_Drive_UI.Domain
                     }
                 }
             }
+        }
+
+        private bool waitForThreadsToFinish()
+        {
+            try
+            {
+                if (Buffer1PollingThread != null)
+                {
+                    while (Buffer1PollingThread.IsAlive)
+                    {
+                        Console.WriteLine(" Threads still alive....");
+                        Thread.Sleep(200);
+                    }
+                }
+                if (Buffer2PollingThread != null)
+                {
+                    while (Buffer2PollingThread.IsAlive)
+                    {
+                        Console.WriteLine(" Threads still alive....");
+                        Thread.Sleep(200);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Failed while waiting for pollingthreads to finish: " + e.Message);
+                return false;
+            }
+            return true;
         }
 
         private bool busy = false;
